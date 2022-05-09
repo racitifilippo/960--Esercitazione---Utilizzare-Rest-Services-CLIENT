3d1b82ec727086af8591c691a7436fc85e1a7067
racitifilippo <raciti.filippo@iisgalvanimi.edu.it>
2022-05-09 12:28:22 +0200

    modifica client

--- Client/src/app/app.component.spec.ts
+++ Client/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Client'`, () => {
+  it(`should have as title 'prova11111'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Client');
+    expect(app.title).toEqual('prova11111');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('prova11111 app is running!');
   });
 });
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewChecked, Component, OnDestroy, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit, OnDestroy {
+  nome:string = "";
+  utenti:string[] = [];
+  socket = io('http://localhost:3002');
+  message$: BehaviorSubject<string> = new BehaviorSubject('');
+
+  ngOnInit(): void {
+    this.socket.on('aggiorna', (arg) => {
+      this.message$.next(arg);
+    });
+    this.message$.asObservable().subscribe((message) => {
+      console.log(message);
+      this.utenti = message.toString().split(',')
+    });
+
+  }
+
+  constructor(private http:HttpClient){
+    this.http.get("https://randomuser.me/api/").subscribe(
+      res => {
+        var x = res["results"][0]["name"];
+        this.nome = x["title"] + " " + x["first"] + " " + x["last"];
+        console.log(this.nome)
+        this.socket.emit("aggiungi", this.nome);
+
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  clickDisconn(){
+    console.log('disconnessione')
+    this.socket.emit('discon', this.nome)
+    this.socket.emit('disconnect', this.nome)
+    window.close()
+  }
+
+  ngOnDestroy(): void {
+    //NON VAAAAAAAAAAAA
+    this.clickDisconn()
+  }
+
+  
 }
