a1a0da360b2fe849097bb9f258a28ca195caa84d
racitifilippo <raciti.filippo@iisgalvanimi.edu.it>
2022-05-09 14:54:55 +0200

    update server

--- ServerNode/Server.js
+++ ServerNode/Server.js
@@ -11,43 +11,35 @@ cors: {origin : '*'}
 });
 
 const port = process.env.PORT || 3002;
-var nomi = []
+var nomi = ['ciao']
+var addr = ['::ffff:192.168.199.117']
 
 
 io.on('connection', (socket) => {
     console.log('ciao')
     socket.on('aggiungi', (arg) => {
         console.log('Aggiunta '+arg)
+        ad = socket.request.connection.remoteAddress
+        console.log('address: '+ad)
+        addr.push(ad)
         nomi.push(arg)
         io.emit('aggiorna', nomi)
         console.log(nomi)
+        console.log(addr)
     })
 
-    socket.on('discon', (arg)=>{
-        console.log('-------------'+arg)
-
-        for (let i = 0 ; i < nomi.length ; i++) {
-            console.log(arg)
-            if (nomi[i] == arg) {
-                console.log('eccoloooooooo')
-                nomi.pop(i)
-            }
-        }
-
-        io.emit('aggiorna', nomi)
-    })
+    
 
     socket.on('disconnect', (arg) => {
         console.log('a user disconnected! --> ' + arg);
         io.emit('discon', 'https://i.redd.it/g4rzf4xvcn871.jpg')
-
-        for (let i = 0 ; i < nomi.length ; i++) {
-            console.log(arg)
-            if (nomi[i] == arg) {
-                console.log('eccoloooooooo')
+        ad = socket.request.connection.remoteAddress
+        for (let i = 0 ; i < addr.length ; i++) {
+            if (addr[i] == ad) {
+                console.log('eccoloooooooo ' + addr[i] + ' ' + ad)
                 nomi.pop(i)
+                addr.pop(i)
             }
-            
         }
     });
 });
